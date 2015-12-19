8725b8d46412cb0f7f18bf990c72b64e5c52316a
ariffurqon <arif.furqon@gmail.com>
2015-12-19 00:06:35 -0800

    flexbox border color cheat sheet

--- index.ios.js
+++ index.ios.js
@@ -1,34 +1,65 @@
 var React = require('react-native');
-
 var {
   Text,
   View,
-  AppRegistry
+  AppRegistry,
+  StyleSheet
 } = React;
 
 var TickTock = React.createClass({
   render: function() {
-    return <View>
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={this.border('red')}>
+          <Text>
+            00:00.00
+          </Text>
+        </View>
+        <View style={this.border('green')}> 
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer, this.border('blue')]}>
         <Text>
-          00:00.00
+          I am a list of Laps
         </Text>
-        {this.startStopButton()}
-        {this.lapButton()}
+      </View>
     </View>
   },
-  startStopButton: function(){
+  startStopButton: function() {
     return <View>
-          <Text>
-            Start
-          </Text>
-      </View>
+      <Text>
+        Start
+      </Text>
+    </View>
   },
-  lapButton: function(){
+  lapButton: function() {
     return <View>
-          <Text>
-            Lap
-          </Text>
-      </View>
+      <Text>
+        Lap
+      </Text>
+    </View>
+  },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire the screen
+    alignItems: 'stretch'
+  },
+  header: { // Yellow
+    flex: 1
+  },
+  footer: { // Blue
+    flex: 1
   }
 });
 
